578a97a551770e1402d1d351d6ee69c0c1f146c8
Duncan Bristow <duncbristow@msn.com>
2017-04-12 09:34:38 -0600

    Added an additional mirror to 'add-testing'

--- add-testing/install.sh
+++ add-testing/install.sh
@@ -8,18 +8,25 @@ command -v netselect-apt >/dev/null 2>&1 || { apt-get -y install netselect-apt; 
 echo "Searching for fastest mirrors... this may take a while."
 
 netselect-apt >/tmp/mirrors.txt 2>/dev/null
-NUM=$((1 + RANDOM % 10 + 1)) #Pick a random mirror from the fastest 10
+NUM1=$((2 + RANDOM % 10)) #Pick a random mirror from the fastest 10
+NUM2=$((2 + RANDOM % 10))
+while [ $NUM2 -eq $NUM1 ]; then #Ensure the numbers are unique
+	NUM2=$((2 + RANDOM % 10))
+done
 
-mirror=$(sed "${NUM}q;d" /tmp/mirrors.txt | tr -d '[:space:]')
+mirror1=$(sed "${NUM}q;d" /tmp/mirrors.txt | tr -d '[:space:]')
+mirror2=$(sed "${NUM}q;d" /tmp/mirrors.txt | tr -d '[:space:]')
 rm /tmp/mirrors.txt sources.list
 
-for f in experimental.list security.list stable.list testing.list unstable.list
+for f in experimental.list stable.list testing.list unstable.list
 do
-	sed -i -e "s|MIRROR|${mirror}|g" "sources.list.d/${f}"
+	sed -i -e "s|MIRROR1|${mirror1}|g" "sources.list.d/${f}"
+	sed -i -e "s|MIRROR2|${mirror2}|g" "sources.list.d/${f}"
 done
 
 mv preferences.d/* /etc/apt/preferences.d/
 mv sources.list.d/* /etc/apt/sources.list.d/
 
-echo "Should be complete! Run apt-get update and look for the following mirror:"
-echo $mirror
+echo "Should be complete! Run apt-get update and look for the following mirrors:"
+echo $mirror1
+echo $mirror2
